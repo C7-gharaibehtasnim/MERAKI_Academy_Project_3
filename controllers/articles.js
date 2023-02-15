00714bffafc3ca09e1d7c9ca1b823d452b6f9c19
Tasneem Gharaibeh <tasnim.gharaibeh@gmail.com>
2023-02-15 20:42:12 +0300

    1. getAllArticles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,9 +1,13 @@
 let articles = require("../models/articles");
 
 // 1. this function return all articles
-const getAllArticles = (req, res) => {
-  
-};
+const  getAllArticles = (req, res) => {
+
+ res.json(articles)
+
+
+
+}
 
 module.exports = {
   getAllArticles,
